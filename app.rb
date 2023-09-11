62980b25bfc809a044124263252f1452122d1a10
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-11 21:19:27 +0630

    Implement add_book function to add books

---
+++ app.rb
@@ -0,0 +1,27 @@
+require_relative 'book'
+require_relative 'label'
+
+class App
+    def self.list_books
+        Book.display
+    end
+
+    def self.add_book
+        puts "Add a book to library"
+        print "Enter Publisher: "
+        publisher = gets.chomp
+        print "Enter Cover State: "
+        cover_state = gets.chomp
+        print "Enter Publish Date: "
+        publish_date = gets.chomp
+        print "Enter Label: "
+        label = gets.chomp
+        label = Label.new(title: label)
+        Book.new(publisher: publisher, cover_state: cover_state, publish_date: publish_date, label: label)
+        puts "Book added successfully"
+    end
+
+    def self.list_all_labels
+
+    end
+end
