d9576378ad999fd9a00077990c2111a936355ee2
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-11 22:18:03 +0100

    Create Game Class

---
+++ game.rb
@@ -0,0 +1,16 @@
+require 'date'
+require_relative '../item'
+
+class Game < Item
+  attr_reader :multiplayer, :last_played_at, :publish_date
+
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
+
+  def can_be_archived?
+    super && (Time.now.year - @last_played_at.year) > 2
+  end
+end
