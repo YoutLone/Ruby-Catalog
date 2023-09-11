37520f08885a1bf3ac4e9f86c5b0d02b10fc5d7f
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-11 22:08:48 +0100

    Create Author class and implement add_item method

--- item.rb
+++ item.rb
@@ -1,26 +1,26 @@
 require 'date'
 
 class Item
-    attr_reader :id
-    attr_accessor :publish_date, :archived
-    def initialize(publish_date:, archived:false)
-        @id = Random.rand(1..1000)
-        @genre = nil
-        @author = nil
-        @label = nil
-        @publish_date = publish_date
-        @archived = archived
-    end
+  attr_reader :id
+  attr_accessor :publish_date, :archived
 
-    def can_be_archived?
-        publish_year = Date.strptime(@publish_date, '%d-%m-%Y');
-        Time.now.year - publish_year.to_i > 10
-    end
+  def initialize(publish_date:, archived: false)
+    @id = Random.rand(1..1000)
+    @genre = nil
+    @author = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def move_to_archive?
-        can_be_archived? == true
-    end
-    
-    private :can_be_archived?
+  def can_be_archived?
+    publish_year = Date.strptime(@publish_date, '%d-%m-%Y');
+    Time.now.year - publish_year.to_i > 10
+  end
 
+  def move_to_archive?
+    can_be_archived? == true
+  end
+
+  private :can_be_archived?
 end
--- main.rb
+++ main.rb
@@ -1,50 +1,50 @@
 puts 'Welcome to Catolog of My Things'.center(50, '#')
-puts ''.center(50,'-')
+puts ''.center(50, '-')
 
 def main
-loop do
-        puts " 1 - List All Books"
-        puts " 2 - List All Music Albums"
-        puts " 3 - List All Games"
-        puts " 4 - List All Genres (e.g 'Comedy', 'Thriller')"
-        puts " 5 - List All Labels  (e.g. 'Gift', 'New') "
-        puts " 6 - List All Authors (e.g. 'Stephen King')"
-        puts " 7 - Add a Book"
-        puts " 8 - Add a Music Album"
-        puts " 9 - Add a Game"
-        puts "10 - Save and Exit"
-        option = gets.chomp.to_i
-        select = Selection.new
-        select.choice_method(option)
-    end
+  loop do
+    puts ' 1 - List All Books'
+    puts ' 2 - List All Music Albums'
+    puts ' 3 - List All Games'
+    puts ' 4 - List All Genres (e.g 'Comedy', 'Thriller')'
+    puts ' 5 - List All Labels  (e.g. 'Gift', 'New')'
+    puts ' 6 - List All Authors (e.g. 'Stephen King')'
+    puts ' 7 - Add a Book'
+    puts ' 8 - Add a Music Album'
+    puts ' 9 - Add a Game'
+    puts ' 10 - Save and Exit '
+    option = gets.chomp.to_i
+    select = Selection.new
+    select.choice_method(option)
+  end
 end
 
 class Selection
-    def choice_method(option)
-        case option
-        when 1 
-            puts "list all books"
-        when 2 
-            puts "list all music albums"
-        when 3
-            puts "list all games"
-        when 4
-            puts "list all genres"
-        when 5 
-            puts "list all labels"
-        when 6 
-            puts "list all authors"
-        when 7 
-            puts "add a book"
-        when 8 
-            puts "add a music album"
-        when 9
-            puts "add a game"
-        when 10 
-            "exit from the program"
-             abort
-        end
+  def choice_method(option)
+    case option
+    when 1
+      puts 'list all books'
+    when 2
+      puts 'list all music albums'
+    when 3
+      puts 'list all games'
+    when 4
+      puts 'list all genres'    
+    when 5
+      puts 'list all labels'
+    when 6
+      puts 'list all authors'
+    when 7
+      puts 'add a book'
+    when 8
+      puts 'add a music album'
+    when 9
+      puts 'add a game'
+    when 10
+      'exit from the program'      
+      abort
     end
+  end
 end
 
 main
