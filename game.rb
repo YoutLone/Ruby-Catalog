b2c41c4c732bdc45ecb91d75edb2ca5c9f75d289
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-12 14:01:24 +0100

    Implemeting add method for creating new game

--- game.rb
+++ game.rb
@@ -3,6 +3,7 @@ require_relative '../item'
 
 class Game < Item
   attr_reader :multiplayer, :last_played_at, :publish_date
+  DATA_FOLDER = 'JSON/'.freeze
 
   def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
@@ -12,5 +13,48 @@ class Game < Item
 
   def can_be_archived?
     super && (Time.now.year - @last_played_at.year) > 2
+  end
+
+  def create_game(multiplayer, last_played_at, publish_date)
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @games << game
+  end
+
+  def list_games
+    if @games.empty?
+      puts 'There are no games'
+    else
+      @games.each_with_index do |game, i|
+        lpa = game.last_played_at
+        puts "#{i + 1}) Multiplayer: #{game.multiplayer}, last played: #{lpa}, published: #{game.publish_date}"
+      end
+    end
+  end
+
+  def save_games(games)
+    File.open("#{DATA_FOLDER}games.json", 'w') do |file|
+      data = {
+        'Games' => games.map do |game|
+          {
+            'multiplayer' => game.multiplayer,
+            'last_played_at' => game.last_played_at,
+            'publish_date' => game.publish_date,
+            'id' => game.id
+          }
+        end
+      }
+      file.write(JSON.pretty_generate(data))
+    end
+  end
+
+  def load_games
+    return [] unless File.exist?("#{DATA_FOLDER}games.json")
+
+    data = JSON.parse(File.read("#{DATA_FOLDER}games.json"))
+    games = []
+    data['Games'].map do |game_data|
+      games << Game.new(game_data['multiplayer'], game_data['last_played_at'], game_data['publish_date'])
+    end
+    games
   end
 end
