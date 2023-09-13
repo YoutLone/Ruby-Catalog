b32e16f522c3699c4bfb444c81caa7bcc157e643
Oumaima Nejjari <oumanj1998@gmail.com>
2023-09-13 14:20:45 +0100

    Writing test units

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,14 +1,20 @@
-require_relative '../author'
-require_relative '../item'
+require_relative 'spec_helper'
 
 describe Author do
-  describe '#initialize' do
-    it 'author must be created' do
-      author = Author.new('Atif', 'Zada')
-
-      expect(author.first_name).to eq('Atif')
-      expect(author.last_name).to eq('Zada')
-      expect(author.items).to be_empty
+  before :each do
+    @author = Author.new(first_name: 'oumaima', last_name: 'nejjari')
+  end
+  describe "#new" do
+    context "when creating a new author" do
+      it 'should be an instance of a author class ' do
+        expect(@author).to be_an_instance_of Author
+      end
+      it 'should return correct first name' do
+        expect(@author.first_name).to eq 'oumaima'
+      end
+      it 'should return correct last name' do
+        expect(@author.last_name).to eq 'nejjari'
+      end
     end
   end
 end
